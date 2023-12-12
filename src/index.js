3353b6bd41e81be149acc29cc4354427aceea663
tcederberg <trcederberg@gmail.com>
2023-12-12 13:53:37 -0500

    atatus integration

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('bdebe556f5c5407fbfa3dc8524a8a477').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
